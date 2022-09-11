659ba658fba30803e04005e6da4287d225b7f2cf
Alex Paris <alex_paris28@hotmail.com>
2022-09-11 08:43:11 -0300

    chore: Todolist component

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,3 +1,4 @@
+import clipboardImg from '../assets/clipboard.svg';
 import styles from "./TodoList.module.css"
 
 interface ITodoListProps {}
@@ -6,9 +7,28 @@ export function TodoList({}: ITodoListProps) {
   return (
     <div className={styles.todos}>
       <header className={styles.header}>
-        <h5 className={styles.createdTasks}>Created tasks</h5>
-        <h5 className={styles.doneTasks}>Done</h5>
+        <div>
+          <h5 className={styles.createdTasks}>Created tasks</h5>
+          <span>4</span>
+        </div>
+        <div>
+          <h5 className={styles.doneTasks}>Done</h5>
+          <span>4</span>
+        </div>
       </header>
+
+      {false ? (
+        <div className={styles.emptyList}>
+          <img src={clipboardImg} />
+          <h4>You don't have any created tasks</h4>
+          <p>Create tasks and organize your to-do items</p>
+        </div>
+      ) : (
+        <div className={styles.filledList}>
+          <h2>lets make a list</h2>
+        </div>
+      )}
+
     </div>
   )
 }
